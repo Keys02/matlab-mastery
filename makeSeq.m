08c9dcd387952562be47bfe671e67bbf7c73ec7b
Keys <keys14052002@gmail.com>
2025-09-02 11:06:43 +0000

    chore: adding comments to sequence maker function

--- makeSeq.m
+++ makeSeq.m
@@ -1,6 +1,5 @@
 function sum = constSeq()
-    sum = 0;
-
+    sum = 0; % Variable which collect all the sum of numbers
     for n = 0:100
         sum =sum + ((-1) ^ n * (pi) ^ 2*n) / factorial(2*n);
     end
